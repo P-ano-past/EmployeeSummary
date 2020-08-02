4ac1254b6a170810cd75d57e99f32c76700f62cd
Marc S <39275810+P-ano-past@users.noreply.github.com>
2020-08-01 19:06:22 -0700

    bloops

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const Manager = require("./EmployeeTemplates/");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("./Render/ManagerRender");
+const Engineer = require("./Render/EngineerRender");
+const Intern = require("./Render/InternRender");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-
+const render = require("./Render/HtmlRender");
 const team = [];
 const idArray = [];
 
